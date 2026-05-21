eee23c7264dfea75387342ab73ebf29602363d90
vlnguyen910 <volamnguyen447@gmail.com>
2026-05-21 21:07:22 +0700

    feat(api): add accounts module with CRUD operations and Prisma integration

---
+++ apps/api/src/modules/accounts/accounts.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { UpdateAccountDto } from './dto/update-account.dto';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class AccountsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createAccountDto: CreateAccountDto) {
+    return 'This action adds a new account';
+  }
+
+  findAll() {
+    return `This action returns all accounts`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} account`;
+  }
+
+  update(id: number, updateAccountDto: UpdateAccountDto) {
+    return `This action updates a #${id} account`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} account`;
+  }
+}
