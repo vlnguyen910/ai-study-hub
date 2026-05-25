8352f6e8df80e573d2d74b9851fac27b57284e3a
itBaowy <notbao.js@gmail.com>
2026-05-25 18:00:59 +0700

    fix(api): auth request validation using class-validator

--- apps/api/src/modules/auth/dto/signin.dto.ts
+++ apps/api/src/modules/auth/dto/signin.dto.ts
@@ -1,7 +1,21 @@
 import type { AuthDeviceInfo } from './signup.dto';
+import {
+  IsEmail,
+  IsIn,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class SigninDto {
+  @IsEmail()
   email!: string;
+
+  @IsString()
+  @MinLength(8)
   password!: string;
+
+  @IsOptional()
+  @IsIn(['WEB', 'MOBILE'])
   deviceInfo?: AuthDeviceInfo;
 }
