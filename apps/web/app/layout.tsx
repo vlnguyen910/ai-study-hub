dd91a14cbfdff7f6362fc081b14c467fb7cbefa8
Võ Đức Huy <huyvdse180565@fpt.edu.vn>
2026-05-26 21:20:34 +0700

    feat: handle conflict local branch & fix some linter UI bug

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -17,13 +17,14 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: ReactNode;
-}>): any {
+}>): React.JSX.Element {
   return (
     <html lang="en">
       <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=block"
         />
       </head>
       <body className={inter.className}>{children}</body>
