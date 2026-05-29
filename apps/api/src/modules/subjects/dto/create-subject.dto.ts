59652f00dbd3bb6bdb3703b3933f935734ad523c
vlnguyen910 <volamnguyen447@gmail.com>
2026-05-29 12:06:12 +0700

    feat(api): enhance seeding process and update subject DTO validation

--- apps/api/src/modules/subjects/dto/create-subject.dto.ts
+++ apps/api/src/modules/subjects/dto/create-subject.dto.ts
@@ -1,12 +1,15 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateSubjectDto {
+  @IsNotEmpty()
   @IsString()
   name!: string;
 
+  @IsNotEmpty()
   @IsString()
   code!: string;
 
+  @IsNotEmpty()
   @IsString()
   schoolId!: string;
 }
