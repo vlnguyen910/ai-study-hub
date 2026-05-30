61e81cb997aee0c17fa951a10b97aae94e5fd119
itzBaowy <notbao.js@gmail.com>
2026-05-30 11:06:02 +0700

    refractor(api): add validation rules to create account dto

--- apps/api/src/modules/accounts/dto/create-account.dto.ts
+++ apps/api/src/modules/accounts/dto/create-account.dto.ts
@@ -1,1 +1,30 @@
-export class CreateAccountDto {}
+import { UserRole } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MinLength,
+} from 'class-validator';
+
+export class CreateAccountDto {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(1)
+  name!: string;
+
+  @IsString()
+  @MinLength(8)
+  password!: string;
+
+  @IsOptional()
+  @IsUrl({ require_tld: false })
+  avatarUrl?: string;
+
+  @IsOptional()
+  @IsEnum(UserRole)
+  role?: UserRole;
+}
