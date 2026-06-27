abe321652e623d3c1e6e3cb01a248af05ed043a8
vlnguyen910 <volamnguyen477@gmail.com>
2026-06-27 21:50:21 +0700

    chore: update package and add nativewind ui

---
+++ apps/mobile/src/components/nativewindui/Icon/types.ts
@@ -0,0 +1,24 @@
+import type MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import type MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import type { SymbolViewProps } from "expo-symbols";
+import type { IconMapper } from "rn-icon-mapper";
+
+type MaterialCommunityIconsProps = React.ComponentProps<
+  typeof MaterialCommunityIcons
+>;
+type MaterialIconsProps = React.ComponentProps<typeof MaterialIcons>;
+
+type Style = SymbolViewProps["style"] &
+  MaterialIconsProps["style"] &
+  MaterialCommunityIconsProps["style"];
+
+type IconProps = IconMapper<
+  Omit<SymbolViewProps, "name"> & { name: string },
+  MaterialIconsProps,
+  MaterialCommunityIconsProps
+> & {
+  style?: Style;
+  className?: string;
+};
+
+export type { IconProps };
